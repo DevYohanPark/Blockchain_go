77fad626ce3324df02b5a67ecafdf63530de80ad
Yohan <yohanpark@medibloc.org>
2018-09-23 10:49:30 +0900

    implement block

--- block.go
+++ block.go
@@ -1,8 +1,29 @@
 package blockchain_go
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
 type Block struct {
 	Timestamp     int64
 	PrevBlockHash []byte
 	Hash          []byte
 	Data          []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{timestamp, b.PrevBlockHash, b.Data}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), prevBlockHash, []byte{}, []byte(data)}
+	block.SetHash()
+	return block
 }
